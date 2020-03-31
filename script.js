643766183bfbc622d4dd7bc15a16bc26f0cea0bc
ajbates2 <60674015+ajbates2@users.noreply.github.com>
2020-03-31 12:38:11 -0500

    grayboxed again

--- script.js
+++
@@ -1,8 +0,0 @@
-function menuFunction() {
-    let click = document.getElementById("nav-links");
-    if (click.style.display === "block") {
-      click.style.display = "none";
-    } else {
-      click.style.display = "block";
-    }
-  }
